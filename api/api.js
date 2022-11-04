d0f7baa91d30d6699e58d604c56d16f0e8dda64c
randymz10 <randymz10@gmail.com>
2022-11-04 00:31:14 -0300

    Add project api

---
+++ api/api.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => { 
+    res.status(200).send('Chanchito feliz');
+});
+
+app.post('/', (req, res) => {
+    res.status(201).send('Creando chanchito');
+});
+
+app.listen(port, () => {
+    console.log('Arrancando la aplicacion');
+});
