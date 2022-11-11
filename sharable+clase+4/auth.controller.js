31d9b4253bc76bdd0b16e9a0e6267c6cb20017d4
randymz10 <randymz10@gmail.com>
2022-11-11 17:45:02 -0300

    Add project Animals

---
+++ sharable+clase+4/auth.controller.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const bcrypt = requir('bcrypt');
+const expressJwt = require('express-jwt');
+const jwt = require('jsonwebtoken');
+const User = require('./user.model');
+
+const validateJwt = expressJwt({secret: process.env.SECRET, algorithms: ['HS256']});
+
+const signToken = _id => jwt.sign({_id}, process.env.SECRET);
+
+const findAndAssignUser = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.user._id);
+        if(!user) {
+            return res.status(401).end();
+        }
+        req.user = user;
+        next();
+    } catch (err) {
+        next(err);
+    }
+}
+
+const isAuthenticated = express.Router().use(validateJwt, findAndAssignUser);
+
+const Auth = {
+    login: async (req, res) => {
+        const { body } = req;
+        try {
+            const user = await User.findOne({email: body.email});
+            if(!user){
+                res.status(401).send('Usuario y/o contraseña inválida');
+            } else {
+                const isMatch = await bcrypt.compare(body.password, user.password);
+                if(isMatch) {
+                    const signed = signToken(user._id);
+                    res.status(200).send(signed);
+                } else {
+                    res.status(401).send('Usuario y/o contraseña inválida');
+                }
+            }
+        } catch (err) {
+            res.send(err.message);
+        }
+    },
+    register: async (req, res) => {
+        const { body } = req;
+        try {
+            const isUser = await User.findOne({ email: body.email});
+            if(isUser) {
+                res.send('usuario ya existe');
+            } else {
+                const salt = await bcrypt.genSalt();
+                const hashed = await bcrypt.hash(body.password, salt);
+                const user = await User.create({email: body.email, password: hashed, salt});
+
+                const signed = signToken(user._id);
+            }
+        } catch (err) {
+            res.status(500).send(err.message);
+        }
+    },
+}
+
+module.exports = { Auth, isAuthenticated};
